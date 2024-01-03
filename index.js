db2b2978ecc4fc69895c1163375ed3ec09c6a77d
Phi Tran <phi@jambowallet.com>
2024-01-03 14:46:54 +0700

    test

--- index.js
+++ index.js
@@ -9,33 +9,16 @@ var privateKey = fs.readFileSync("privateKey.pem");
 
 var token = jwt.sign(
   {
-    iss: "https://securetoken.google.com/jambo-4e857",
-    aud: "jambo-4e857",
-    auth_time: 1703173063,
-    user_id: "MF4NP9TjRKNPeJzDKpOg5dqPCUz1",
-    sub: "MF4NP9TjRKNPeJzDKpOg5dqPCUz1",
-    iat: 1703173064,
-    exp: 1703176664,
-    email: "phi@jambo.technology",
-    email_verified: false,
-    firebase: {
-      identities: {
-        email: ["phi@jambo.technology"],
-      },
-      sign_in_provider: "password",
-    },
+    sub: "116546237406834305867", // must be unique to each user
+    name: "Huỳnh Ái Tâm Như",
+    email: "epshoesshop@gmail.com",
+    aud: "763348850111-fmj2fadg9f435t471cc31fke8mvg0aqa.apps.googleusercontent.com", // -> to be used in Custom Authentication as JWT Field
+    iss: "https://accounts.google.com", // -> to be used in Custom Authentication as JWT Field
+    iat: Math.floor(Date.now() / 1000),
+    exp: Math.floor(Date.now() / 1000) + 60 * 60,
   },
-  // {
-  //   sub: "100598431157052412238", // must be unique to each user
-  //   name: "Quảng Phi Trần",
-  //   email: "tranquangphi1997@gmail.com",
-  //   aud: "47479090618-iqll0rpmik55bdg9kcjo6o7bju1ql9oo.apps.googleusercontent.com", // -> to be used in Custom Authentication as JWT Field
-  //   iss: "https://accounts.google.com", // -> to be used in Custom Authentication as JWT Field
-  //   iat: Math.floor(Date.now() / 1000),
-  //   exp: Math.floor(Date.now() / 1000) + 60 * 60,
-  // },
   privateKey,
-  { algorithm: "RS256", keyid: "032cc1cb289dd4626a435d72989ae43212defe78" } // <-- Replace it with your kid. This has to be present in the JWKS endpoint.
+  { algorithm: "RS256", keyid: "91413cf4fa0cb92a3c3f5a054509132c47660937" } // <-- Replace it with your kid. This has to be present in the JWKS endpoint.
 );
 
 console.log(token);
